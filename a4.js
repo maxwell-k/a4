6e592a5e7bb6f76b47ce13c5c32765fd124b18fe
Keith Maxwell <keith.maxwell@gmail.com>
2022-05-08 11:31:27 +0100

    Format a4.js with prettier@2.6.2
    Specifically:
    
        npm exec -- prettier@2.6.2 -w a4.js

--- a4.js
+++ a4.js
@@ -19,7 +19,7 @@ const numbering = {
       text-align: center;
     ">
       Page <span class="pageNumber"></span> of <span class="totalPages"></span>
-    </div>`
+    </div>`,
 };
 var input;
 var pdf;
@@ -46,7 +46,7 @@ if (!process.argv.slice(2).length) {
   process.exit(1);
 }
 var flagged = false;
-let exit = message => {
+let exit = (message) => {
   console.error(message);
   flagged = true;
 };
@@ -65,7 +65,7 @@ if (flagged) {
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/chromium-browser"
+    executablePath: "/usr/bin/chromium-browser",
   });
   const page = await browser.newPage();
   const url = http ? `${input}` : `file:///${resolve(input)}`;
@@ -77,9 +77,9 @@ if (flagged) {
       format: program.format,
       landscape: program.landscape || false,
       /* margins set to 25mm to match Microsoft Word Online defaults */
-      margin: { top: "25mm", right: "25mm", bottom: "25mm", left: "25mm" }
+      margin: { top: "25mm", right: "25mm", bottom: "25mm", left: "25mm" },
     },
-    ...(program.number ? numbering : {})
+    ...(program.number ? numbering : {}),
   });
 
   await browser.close();
