691574e17f61c4b88aeb62ec2e643ec2c8b7215d
Keith Maxwell <keith.maxwell@gmail.com>
2022-06-20 11:33:35 +0100

    Upgrade commander
    - In version 7 there was a breaking change
      https://github.com/tj/commander.js/blob/master/CHANGELOG.md#700-2021-01-15
    - By default, options are no longer available on program
      https://github.com/tj/commander.js/pull/1409
    - Updated code to follow the quick start example
      https://github.com/tj/commander.js#quick-start

--- a4.js
+++ a4.js
@@ -65,6 +65,7 @@ if (flagged) {
 }
 
 (async () => {
+  const options = program.opts();
   const browser = await puppeteer.launch({
     executablePath: paths.filter(existsSync)[0],
   });
@@ -75,12 +76,12 @@ if (flagged) {
   await page.pdf({
     ...{
       path: pdf,
-      format: program.format,
-      landscape: program.landscape || false,
+      format: options.format,
+      landscape: options.landscape || false,
       /* margins set to 25mm to match Microsoft Word Online defaults */
       margin: { top: "25mm", right: "25mm", bottom: "25mm", left: "25mm" },
     },
-    ...(program.number ? numbering : {}),
+    ...(options.number ? numbering : {}),
   });
 
   await browser.close();
