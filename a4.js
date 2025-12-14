f63211a0eebf1c98a9da34100edbc12364fa2b65
Keith Maxwell <keith.maxwell@gmail.com>
2025-12-14 11:19:30 +0000

    Fix version numbers to match latest tag

--- a4.js
+++ a4.js
@@ -28,7 +28,7 @@ let input;
 let pdf;
 
 program
-  .version("0.1.1")
+  .version("0.2.0")
   .arguments(`<source.(${extensions})> [destination.pdf]`)
   .action((source, destination) => {
     input = source;
